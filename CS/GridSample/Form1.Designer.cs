4c6637956c496564164212d900e62424fa58ff51
SvetlanaPN <svetlana.peksheva@devexpress.com>
2020-07-15 21:15:44 +0300

    use dragdropbehavior

--- CS/GridSample/Form1.Designer.cs
+++ CS/GridSample/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             DevExpress.XtraGrid.Views.Tile.TileViewItemElement tileViewItemElement1 = new DevExpress.XtraGrid.Views.Tile.TileViewItemElement();
             DevExpress.XtraGrid.Views.Tile.TileViewItemElement tileViewItemElement2 = new DevExpress.XtraGrid.Views.Tile.TileViewItemElement();
             DevExpress.XtraGrid.Views.Tile.TileViewItemElement tileViewItemElement3 = new DevExpress.XtraGrid.Views.Tile.TileViewItemElement();
@@ -41,68 +42,82 @@
             this.tileView1 = new DevExpress.XtraGrid.Views.Tile.TileView();
             this.gridControl2 = new DevExpress.XtraGrid.GridControl();
             this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            this.dragDropEvents1 = new DevExpress.Utils.DragDrop.DragDropEvents(this.components);
+            this.dragDropEvents2 = new DevExpress.Utils.DragDrop.DragDropEvents(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
             this.splitContainerControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tileView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
             this.SuspendLayout();
             // 
             // tileViewColumn1
             // 
             this.tileViewColumn1.Caption = "tileViewColumn1";
             this.tileViewColumn1.FieldName = "Dosage";
+            this.tileViewColumn1.MinWidth = 17;
             this.tileViewColumn1.Name = "tileViewColumn1";
             this.tileViewColumn1.Visible = true;
             this.tileViewColumn1.VisibleIndex = 0;
+            this.tileViewColumn1.Width = 64;
             // 
             // tileViewColumn2
             // 
             this.tileViewColumn2.Caption = "tileViewColumn2";
             this.tileViewColumn2.FieldName = "Drug";
+            this.tileViewColumn2.MinWidth = 17;
             this.tileViewColumn2.Name = "tileViewColumn2";
             this.tileViewColumn2.Visible = true;
             this.tileViewColumn2.VisibleIndex = 1;
+            this.tileViewColumn2.Width = 64;
             // 
             // tileViewColumn3
             // 
             this.tileViewColumn3.Caption = "tileViewColumn3";
             this.tileViewColumn3.FieldName = "Patient";
+            this.tileViewColumn3.MinWidth = 17;
             this.tileViewColumn3.Name = "tileViewColumn3";
             this.tileViewColumn3.Visible = true;
             this.tileViewColumn3.VisibleIndex = 2;
+            this.tileViewColumn3.Width = 64;
             // 
             // tileViewColumn4
             // 
             this.tileViewColumn4.Caption = "tileViewColumn4";
             this.tileViewColumn4.FieldName = "Date";
+            this.tileViewColumn4.MinWidth = 17;
             this.tileViewColumn4.Name = "tileViewColumn4";
             this.tileViewColumn4.Visible = true;
             this.tileViewColumn4.VisibleIndex = 3;
+            this.tileViewColumn4.Width = 64;
             // 
             // splitContainerControl1
             // 
             this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.splitContainerControl1.Name = "splitContainerControl1";
             this.splitContainerControl1.Panel1.Controls.Add(this.gridControl1);
             this.splitContainerControl1.Panel1.Text = "Panel1";
             this.splitContainerControl1.Panel2.Controls.Add(this.gridControl2);
             this.splitContainerControl1.Panel2.Text = "Panel2";
-            this.splitContainerControl1.Size = new System.Drawing.Size(1244, 460);
-            this.splitContainerControl1.SplitterPosition = 616;
+            this.splitContainerControl1.Size = new System.Drawing.Size(1066, 374);
+            this.splitContainerControl1.SplitterPosition = 528;
             this.splitContainerControl1.TabIndex = 0;
             this.splitContainerControl1.Text = "splitContainerControl1";
             // 
             // gridControl1
             // 
-            this.gridControl1.AllowDrop = true;
             this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.tileView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(616, 460);
+            this.gridControl1.Size = new System.Drawing.Size(528, 374);
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.tileView1});
@@ -113,13 +128,17 @@
             this.tileView1.Appearance.ItemHovered.Options.UseBackColor = true;
             this.tileView1.Appearance.ItemNormal.BackColor = System.Drawing.Color.Gainsboro;
             this.tileView1.Appearance.ItemNormal.Options.UseBackColor = true;
+            this.behaviorManager1.SetBehaviors(this.tileView1, new DevExpress.Utils.Behaviors.Behavior[] {
+            ((DevExpress.Utils.Behaviors.Behavior)(DevExpress.Utils.DragDrop.DragDropBehavior.Create(typeof(DevExpress.XtraGrid.Extensions.ColumnViewDragDropSource), true, true, true, true, this.dragDropEvents1)))});
             this.tileView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
             this.tileViewColumn1,
             this.tileViewColumn2,
             this.tileViewColumn3,
             this.tileViewColumn4});
+            this.tileView1.DetailHeight = 284;
             this.tileView1.GridControl = this.gridControl1;
             this.tileView1.Name = "tileView1";
+            this.tileView1.OptionsBehavior.AllowMousePanning = false;
             tileViewItemElement1.Column = this.tileViewColumn1;
             tileViewItemElement1.Text = "tileViewColumn1";
             tileViewItemElement2.Column = this.tileViewColumn2;
@@ -135,26 +154,30 @@
             // 
             // gridControl2
             // 
-            this.gridControl2.AllowDrop = true;
             this.gridControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl2.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridControl2.Location = new System.Drawing.Point(0, 0);
             this.gridControl2.MainView = this.gridView2;
+            this.gridControl2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridControl2.Name = "gridControl2";
-            this.gridControl2.Size = new System.Drawing.Size(623, 460);
+            this.gridControl2.Size = new System.Drawing.Size(528, 374);
             this.gridControl2.TabIndex = 1;
             this.gridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView2});
             // 
             // gridView2
             // 
+            this.behaviorManager1.SetBehaviors(this.gridView2, new DevExpress.Utils.Behaviors.Behavior[] {
+            ((DevExpress.Utils.Behaviors.Behavior)(DevExpress.Utils.DragDrop.DragDropBehavior.Create(typeof(DevExpress.XtraGrid.Extensions.ColumnViewDragDropSource), true, true, true, true, this.dragDropEvents2)))});
+            this.gridView2.DetailHeight = 284;
             this.gridView2.GridControl = this.gridControl2;
             this.gridView2.Name = "gridView2";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1244, 460);
+            this.ClientSize = new System.Drawing.Size(1066, 374);
             this.Controls.Add(this.splitContainerControl1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
@@ -165,6 +188,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.tileView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -180,35 +204,9 @@
         private DevExpress.XtraGrid.Columns.TileViewColumn tileViewColumn2;
         private DevExpress.XtraGrid.Columns.TileViewColumn tileViewColumn3;
         private DevExpress.XtraGrid.Columns.TileViewColumn tileViewColumn4;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
+        private DevExpress.Utils.DragDrop.DragDropEvents dragDropEvents1;
+        private DevExpress.Utils.DragDrop.DragDropEvents dragDropEvents2;
     }
 }
 
--- CS/GridSample/Form1.cs
+++ CS/GridSample/Form1.cs
@@ -1,37 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Globalization;
 using DevExpress.XtraEditors;
-using DevExpress.XtraEditors.Repository;
 using DevExpress.XtraGrid;
-using DevExpress.XtraGrid.Views.Grid;
-using DevExpress.XtraGrid.Views.Tile.ViewInfo;
-using DevExpress.XtraGrid.Views.Tile;
-using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraGrid.Views.Base;
 
-
-namespace GridSample
-{
-    public partial class Form1 : XtraForm
-    {
-        TileViewHitInfo tileDownHitInfo = null;
-        GridHitInfo gridDownHitInfo = null;
-        public Form1()
-        {
+namespace GridSample {
+    public partial class Form1 : XtraForm {
+        public Form1() {
             InitializeComponent();
 
-            SetUpTileGrid(gridControl1);
+            SetUpGrid(gridControl1);
             SetUpGrid(gridControl2);
             gridControl1.DataSource = GetTable();
             gridControl2.DataSource = GetTable();
         }
-
         static DataTable GetTable() {
             DataTable table = new DataTable();
             table.Columns.Add("Dosage", typeof(int));
@@ -46,94 +29,9 @@ namespace GridSample
             table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
             return table;
         }
-
-        public void SetUpTileGrid(GridControl grid) {
-            grid.AllowDrop = true;
-            grid.DragOver += new System.Windows.Forms.DragEventHandler(grid_DragOver);
-            grid.DragDrop += new System.Windows.Forms.DragEventHandler(grid_DragDrop);
-            TileView view = grid.MainView as TileView;
-            view.OptionsBehavior.Editable = false;
-            view.MouseMove += new System.Windows.Forms.MouseEventHandler(tileView_MouseMove);
-            view.MouseDown += new System.Windows.Forms.MouseEventHandler(tileView_MouseDown);
-        }
-
         public void SetUpGrid(GridControl grid) {
-            grid.AllowDrop = true;
-            grid.DragOver += new System.Windows.Forms.DragEventHandler(grid_DragOver);
-            grid.DragDrop += new System.Windows.Forms.DragEventHandler(grid_DragDrop);
-            GridView view = grid.MainView as GridView;
+            ColumnView view = grid.MainView as ColumnView;
             view.OptionsBehavior.Editable = false;
-            view.MouseMove += new System.Windows.Forms.MouseEventHandler(gridView_MouseMove);
-            view.MouseDown += new System.Windows.Forms.MouseEventHandler(gridView_MouseDown);
-        }
-        
-        private void tileView_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e) {
-            TileView view = sender as TileView;
-            if (view == null) return;
-            tileDownHitInfo = null;
-            TileViewHitInfo hitInfo = view.CalcHitInfo(new Point(e.X, e.Y));
-            if (Control.ModifierKeys != Keys.None) return;
-            if (e.Button == MouseButtons.Left && hitInfo.RowHandle >= 0)
-                tileDownHitInfo = hitInfo;
-        }
-
-        private void tileView_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e) {
-            TileView view = sender as TileView;
-            if (view == null) return;
-            if (e.Button == MouseButtons.Left && tileDownHitInfo != null) {
-                Size dragSize = SystemInformation.DragSize;
-                Rectangle dragRect = new Rectangle(new Point(tileDownHitInfo.HitPoint.X - dragSize.Width / 2,
-                    tileDownHitInfo.HitPoint.Y - dragSize.Height / 2), dragSize);
-
-                if (!dragRect.Contains(new Point(e.X, e.Y))) {
-                    DataRow row = view.GetDataRow(tileDownHitInfo.RowHandle);
-                    view.GridControl.DoDragDrop(row, DragDropEffects.Move);
-                    tileDownHitInfo = null;
-                    DevExpress.Utils.DXMouseEventArgs.GetMouseArgs(e).Handled = true;
-                }
-            }
-        }
-        private void gridView_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e) {
-            GridView view = sender as GridView;
-            if (view == null) return;
-            gridDownHitInfo = null;
-            GridHitInfo hitInfo = view.CalcHitInfo(new Point(e.X, e.Y));
-            if (Control.ModifierKeys != Keys.None) return;
-            if (e.Button == MouseButtons.Left && hitInfo.RowHandle >= 0)
-                gridDownHitInfo = hitInfo;
-        }
-
-        private void gridView_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e) {
-            GridView view = sender as GridView;
-            if (view == null) return;
-            if (e.Button == MouseButtons.Left && gridDownHitInfo != null) {
-                Size dragSize = SystemInformation.DragSize;
-                Rectangle dragRect = new Rectangle(new Point(gridDownHitInfo.HitPoint.X - dragSize.Width / 2,
-                    gridDownHitInfo.HitPoint.Y - dragSize.Height / 2), dragSize);
-
-                if (!dragRect.Contains(new Point(e.X, e.Y))) {
-                    DataRow row = view.GetDataRow(gridDownHitInfo.RowHandle);
-                    view.GridControl.DoDragDrop(row, DragDropEffects.Move);
-                    gridDownHitInfo = null;
-                    DevExpress.Utils.DXMouseEventArgs.GetMouseArgs(e).Handled = true;
-                }
-            }
-        }
-        private void grid_DragOver(object sender, System.Windows.Forms.DragEventArgs e) {
-            if (e.Data.GetDataPresent(typeof(DataRow)))
-                e.Effect = DragDropEffects.Move;
-            else
-                e.Effect = DragDropEffects.None;
-        }
-
-        private void grid_DragDrop(object sender, System.Windows.Forms.DragEventArgs e) {
-            GridControl grid = sender as GridControl;
-            DataTable table = grid.DataSource as DataTable;
-            DataRow row = e.Data.GetData(typeof(DataRow)) as DataRow;
-            if (row != null && table != null && row.Table != table) {
-                table.ImportRow(row);
-                row.Delete();
-            }
         }
     }
 }
