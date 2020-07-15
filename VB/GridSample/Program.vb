a0c0ba7610818ab287a9825e7e1011c8104f1454
DevExpressExampleBot <fakeEmail>
2020-07-15 21:20:25 +0300

    Source auto update [skip ci]

--- VB/GridSample/Program.vb
+++ VB/GridSample/Program.vb
@@ -6,21 +6,17 @@ Imports System.Threading
 Imports System.Windows.Forms
 
 Namespace GridSample
-    Friend NotInheritable Class Program
-
-        Private Sub New()
-        End Sub
-
-        ''' <summary>
-        ''' The main entry point for the application.
-        ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-            Application.EnableVisualStyles()
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			Application.EnableVisualStyles()
 
 
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Form1())
-        End Sub
-    End Class
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
 End Namespace
